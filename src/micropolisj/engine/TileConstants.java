b2511544150d269c4c46259f5b13bf7066708652
Sarah Wang <sa.wang.612@gmail.com>
2019-04-04 05:57:22 -0400

    Generated correct Museum tiles, museum now shows up in game properly

--- src/micropolisj/engine/TileConstants.java
+++ src/micropolisj/engine/TileConstants.java
@@ -150,8 +150,8 @@ public class TileConstants
 	static final char VBRDG1 = 949;
 	static final char VBRDG2 = 950;
 	static final char VBRDG3 = 951;
-	static final char MUSEUM = 966; //Tile id for the MUSEUM tile. This is the id of the center of the tile as listed in tiles.rc
-	public static final char LAST_TILE = 968;
+	static final char MUSEUM = 965; //Tile id for the MUSEUM tile. This is the id of the center of the tile as listed in tiles.rc
+	public static final char LAST_TILE = 975;
 
 	static final char [] RoadTable = new char[] {
 		ROADS, ROADS2, ROADS, ROADS3,
